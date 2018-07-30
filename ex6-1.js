0ad397ad3787d5df8524903e2acee3e8e64d7c40
zm17421 <zm17421@josai.ac.jp>
2018-07-29 20:53:52 -0400

    message

---
+++ ex6-1.js
@@ -0,0 +1,11 @@
+function sayhello(){alert('Hello,Function!');}
+var who='world';
+function sayhello(){
+  alert('Hello,'+who+'!');
+}
+function taro(){
+  who='太郎';
+}
+function hanako(){
+  who='花子';
+}
---
+++ ex6-5.js
@@ -0,0 +1,10 @@
+var who = 'world';
+function sayhello65(){
+  alert('Hello,' + who + '!');
+}
+function taro(){
+  who = '太郎';
+}
+function hanako(){
+  who = '花子';
+}
