51ab0615c485ba3a523075a59e4977d8b2210b4f
zm17421 <zm17421@josai.ac.jp>
2018-08-10 08:09:52 -0400

    message

--- myjs.js
+++ myjs.js
@@ -1,11 +1,5 @@
 // alert('Hello,Iwasa!');
-alert(1+1);
-alert(Math.sqrt(1+1));
-alert(Math.sqrt(2)+Math.sqrt(3));
-alert(1/0);
-alert(0/0);
-alert(2+3);
-alert("2+3");
-alert(Number('2')+Number('3'));
-alert('2'+3)
-alert(Number('2')+3)
+
+function showimg1(){
+  document.area1.src = "IMG_0325.JPG";
+}
